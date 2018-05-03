ce78b8634774fdf1fcc3b1f68181edab45e3c479
Lucie Lemarchand <lucie.lemarchand@he-arc.ch>
2018-05-03 18:25:19 +0200

    implementation de la classe joueur

--- IHCS/include/Joueur.h
+++ IHCS/include/Joueur.h
@@ -1,16 +1,24 @@
 #ifndef JOUEUR_H
 #define JOUEUR_H
 
+#include <string>
+using namespace std;
 
 class Joueur
 {
     public:
         Joueur();
+        Joueur(string nom,int nbButs,int level);
+        Joueur(const Joueur &source);
+        void afficher();
         virtual ~Joueur();
 
     protected:
 
     private:
+        std::string nom;
+        int nbButs;
+        int level;
 };
 
 #endif // JOUEUR_H
--- IHCS/src/Joueur.cpp
+++ IHCS/src/Joueur.cpp
@@ -1,8 +1,35 @@
 #include "Joueur.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
 
 Joueur::Joueur()
 {
-    //ctor
+    this->nom = "no name";
+    this->nbButs = 0;
+    this->level = 0;
+}
+
+Joueur::Joueur(string nom,int nbButs,int level)
+{
+    this->nom = nom;
+    this->nbButs = nbButs;
+    this->level = level;
+}
+
+Joueur::Joueur(const Joueur &source)
+{
+    this->nom = source.nom;
+    this->nbButs = source.nbButs;
+    this->level = source.level;
+}
+
+void Joueur::afficher()
+{
+    cout << "nom : " << nom << endl;
+    cout << "nombre de buts : " << nbButs << endl;
+    cout << "niveau : " << level << endl;
 }
 
 Joueur::~Joueur()
