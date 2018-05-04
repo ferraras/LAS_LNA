0adef1664bf4530fc601c34cf78704fe76281fa9
ferraras <stelvio.ferrara@he-arc.ch>
2018-05-04 10:43:00 +0200

    Ajout du header Equipe

--- IHCS/include/Equipe.h
+++ IHCS/include/Equipe.h
@@ -1,16 +1,32 @@
 #ifndef EQUIPE_H
 #define EQUIPE_H
 
+#include <Joueur.h>
+#include <string>
+#include <list>
+using namespace std;
 
 class Equipe
 {
     public:
+        //Constructeurs
         Equipe();
         virtual ~Equipe();
+
+        //Méthodes
+        void Engager(Joueur joueur);
+        void Afficher();
+
+        //Attributs
+        std::string nom;
+        std::list<int> palmares;
+        short ID;
 
     protected:
 
     private:
+        //Attributs
+        int niveauEquipe;
 };
 
 #endif // EQUIPE_H
